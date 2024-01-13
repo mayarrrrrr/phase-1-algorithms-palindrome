4bbd1569e845d4b9d1131b1de3d96efa8a10a716
mayarrrrrr <akokmayar607@gmail.com>
2024-01-13 19:17:16 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+
+  if (word.length === 0 || word.toUpperCase() === word){ 
+    return 'Cannot return an empty word or word in lowercase'
+  }
+
+  if (typeof word !== "string"){
+    return "Word should be a string"
+  }
+
+  
+  // 
+  for (let i = 0; i <= Math.floor(word.length / 2); i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+  }
+
+
+
+let result = isPalindrome("noon")
+console.log(result);
 
 /* 
   Add your pseudocode here
+  
+  create a function - checks whether word is a palindrome or not
+  pass a string as an arguement
+  loop through every character to see if the forward version matches the backword
+  word - should be in a lowercase
+         -should not be empty
+         - should be a string
+  mum => mum
+
 */
 
 /*
@@ -22,4 +53,4 @@ if (require.main === module) {
   console.log("=>", isPalindrome("robot"));
 }
 
-module.exports = isPalindrome;
+module.export = isPalindrome;
